bb07f9b098effc82eaeaea2fc161104dd83dfef9
Daniel Hampton <neomachina2011@gmail.com>
2017-05-09 12:29:21 -0400

    Finally got a site working at surge.sh

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Hello World</h1>
+        <h2>The app is actually running on Surge</h2>
       </div>
     );
   }
